78f8f95fd9f8c9b4057682856c1d5ac866011020
Kira1108 <327485253@163.com>
2023-03-16 22:51:33 +0800

    Initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(name='simple_chat',
       version='0.0.1',
-      description='A wrapper of openai gpt-3.5 api',
+      description='A wrapper of openai gpt api',
       author='The fastest man alive.',
       packages=find_packages(),
       install_requires=["openai>=0.27.0","rich"])
